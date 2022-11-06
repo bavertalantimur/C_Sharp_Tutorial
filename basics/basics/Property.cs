1bd748612db291db1c73def6399735e1d5d72428
baver <bawer.talantimur@gmail.com>
2022-11-06 15:28:25 +0300

    property process

---
+++ basics/basics/Property.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basics
+{
+    internal class Property
+    {
+        private int id = 5;
+
+        public int age { get; set; }
+        
+        private int tc = 6;
+        public int StudentId
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+
+    }
+}
